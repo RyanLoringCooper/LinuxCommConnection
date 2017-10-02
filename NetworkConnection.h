6e414e1396e1b8c20db3d83cd60706312ca4b17e
Pillager225 <ryanloringcooper@gmail.com>
2017-10-02 15:13:19 -0700

    tcp part of windows written, untested

--- NetworkConnection.h
+++ NetworkConnection.h
@@ -20,6 +20,8 @@
 #include <ws2tcpip.h>
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
 
 #else
 #error Unsupported os
@@ -35,7 +37,8 @@ protected:
 	int mSocket, clientSocket;
 	struct sockaddr_in connAddr;
 #elif defined(_WIN32)
-	SOCKET listenSocket, clientSocket;
+	SOCKET mSocket, clientSocket;
+    struct addrinfo *result;
 #else
 #error Unsupported os
 #endif
--- WindowsNetworkConnection.cpp
+++ WindowsNetworkConnection.cpp
@@ -3,11 +3,16 @@
 // protected
 bool NetworkConnection::setupServer(const int &port) {
     WSADATA wsaData;
-    struct addrinfo *result = NULL, *ptr = NULL, hints;
+    result = NULL;
+    struct addrinfo *ptr = NULL, hints;
     ZeroMemory(&hints, sizeof (hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = connectionType;
-    hints.ai_protocol = IPPROTO_TCP;
+    if(connectionType == SOCK_STREAM) {
+        hints.ai_protocol = IPPROTO_TCP;
+    } else {
+        hints.ai_protocol = IPPROTO_UDP;
+    }
     hints.ai_flags = AI_PASSIVE;
     // Initialize Winsock
     int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -23,26 +28,21 @@ bool NetworkConnection::setupServer(const int &port) {
         return false;
     }
     // setup socket
-    listenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (listenSocket == INVALID_SOCKET) {
+    mSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (mSocket == INVALID_SOCKET) {
         printf("Error at socket(): %ld\n", WSAGetLastError());
         freeaddrinfo(result);
         freeaddrinfo(ptr);
         WSACleanup();
         return false;
     }   
-    BOOL optVal = TRUE;
-    iResult = setsockopt(listenSocket, IPPROTO_TCP, TCP_NODELAY, (char *) &optVal, sizeof(BOOL));
-    if (iResult == SOCKET_ERROR) {
-        printf("getsockopt for TCP_NODELAY failed with error: %u\n", WSAGetLastError());
-    }
     // Setup the TCP listening socket
-    iResult = bind(listenSocket, result->ai_addr, (int)result->ai_addrlen);
+    iResult = bind(mSocket, result->ai_addr, (int)result->ai_addrlen);
     if (iResult == SOCKET_ERROR) {
         printf("bind failed with error: %d\n", WSAGetLastError());
         freeaddrinfo(result);
         freeaddrinfo(ptr);
-        closesocket(listenSocket);
+        closesocket(mSocket);
         WSACleanup();
         return false;
     }
@@ -53,28 +53,64 @@ bool NetworkConnection::setupServer(const int &port) {
 }
 
 bool NetworkConnection::setupClient(const char *ipaddr, const int &port) {
-    // TODO handle as client
+    // Initialize Winsock
+    WSADATA wsaData;
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        fprintf(stderr, "WSAStartup failed with error: %d\n", iResult);
+        return return false;
+    }
+
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = connectionType;
+    if(connectionType == SOCK_STREAM) {
+        hints.ai_protocol = IPPROTO_TCP;
+    } else {
+        hints.ai_protocol = IPPROTO_UDP;
+    }
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(ipaddr, itoa(port), &hints, &result);
+    if ( iResult != 0 ) {
+        fprintf(stderr, "getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return false;
+    }
+    mSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (mSocket == INVALID_SOCKET) {
+        fprintf(stderr, "socket failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+    return connectToServer();
 }
 
-bool NetworkConnection::connectToServer() { // TODO
+bool NetworkConnection::connectToServer() { 
+    iResult = connect(mSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        closesocket(mSocket);
+        mSocket = INVALID_SOCKET;
+        return false;
+    }
     connected = true;
     return true;
 }
 
 bool NetworkConnection::waitForClientConnection() {
     // setup listening on socket for connections
-    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
+    if (listen(mSocket, SOMAXCONN) == SOCKET_ERROR) {
         printf( "Listen failed with error: %ld\n", WSAGetLastError() );
-        closesocket(listenSocket);
+        closesocket(mSocket);
         WSACleanup();
         return false;
     }
     printf("Waiting for controller client connection...\n");
     // accept a client socket
-    clientSocket = accept(listenSocket, NULL, NULL);
+    clientSocket = accept(mSocket, NULL, NULL);
     if (clientSocket == INVALID_SOCKET) {
         printf("accept failed: %d\n", WSAGetLastError());
-        closesocket(listenSocket);
+        closesocket(mSocket);
         WSACleanup();
         return false;
     } 
@@ -86,20 +122,27 @@ bool NetworkConnection::waitForClientConnection() {
 void NetworkConnection::exitGracefully() {
     // shutdown the send half of the connection since no more data will be sent
     if(connected) {
-        int iResult = shutdown(clientSocket, SD_SEND);
-        if (iResult == SOCKET_ERROR) {
-            printf("shutdown failed: %d\n", WSAGetLastError());
+        if(clientSocket != INVALID_SOCKET) {
+            int iResult = shutdown(clientSocket, SD_SEND);
+            if (iResult == SOCKET_ERROR) {
+                fprintf(stderr, "shutdown failed: %d\n", WSAGetLastError());
+            }
+            // cleanup
+            closesocket(clientSocket);
         }
-        // cleanup
-        closesocket(clientSocket);
-        closesocket(listenSocket);
+        closesocket(mSocket);
     }
     WSACleanup();
 }
 
-int NetworkConnection::getData(char *buff, const int &buffSize) { // TODO handle as client
-    if(connected)
-        return recv(clientSocket, buff, buffSize, 0);
+int NetworkConnection::getData(char *buff, const int &buffSize) { 
+    if(connected) {
+        if(clientSocket != INVALID_SOCKET) {
+            return recv(clientSocket, buff, buffSize, 0);
+        } else {
+            return recv(mSocket, buff, buffSize, 0);
+        }
+    }
     return -1;
 }
 
@@ -112,12 +155,16 @@ void NetworkConnection::failedRead() {
 }
 
 // public 
-bool NetworkConnection::write(char *buff, const int &buffSize) { // TODO handle as client
+bool NetworkConnection::write(char *buff, const int &buffSize) { 
 	if(!connected) 
 		return false;
-    int iSendResult = send(clientSocket, buff, buffSize, 0);
+    int iSendResult;
+    if(clientSocket != INVALID_SOCKET) {
+        iSendResult = send(clientSocket, buff, buffSize, 0);
+    } else {
+        iSendResult = send(mSocket, buff, buffSize, 0);
     if(iSendResult == SOCKET_ERROR) {
-        printf("Failed to send: %s", buff);
+        fprintf(stderr, "Failed to send: %s", buff);
         return false;
     }
     return true;
